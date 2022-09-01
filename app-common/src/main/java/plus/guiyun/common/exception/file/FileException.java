f1287df0a0e82f4e4ad69971d906c817ffd0d5f1
归云 <i@guiyunweb.com>
2022-09-01 11:12:37 +0800

    x

---
+++ app-common/src/main/java/plus/guiyun/common/exception/file/FileException.java
@@ -0,0 +1,19 @@
+package plus.guiyun.common.exception.file;
+
+import plus.guiyun.common.exception.base.BaseException;
+
+/**
+ * 文件信息异常类
+ * 
+ * @author ruoyi
+ */
+public class FileException extends BaseException
+{
+    private static final long serialVersionUID = 1L;
+
+    public FileException(String code, Object[] args)
+    {
+        super("file", code, args, null);
+    }
+
+}
